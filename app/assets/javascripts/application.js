c94d6d9da77cb8af3f7137b3d98f021107179d52
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-09-18 17:55:51 +0100

    Beautify input box, functionality and create custom scope

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,12 +23,21 @@ scroll_botton = () => {
   }
 };
 
+submit_message = () => {
+  $("#message_body").on("keydown", (e) => {
+    if (e.keyCode == 13) {
+      $("button").click();
+      e.target.value = "";
+    }
+  });
+};
+
 $(document).on("turbolinks:load", () => {
   $(".ui.dropdown").dropdown();
 
   $(".message .close").on("click", function () {
     $(this).closest(".message").transition("fade");
   });
-
+  submit_message();
   scroll_botton();
 });

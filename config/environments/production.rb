f595a6d30eed0abede33d9fe91728f21a3865e7a
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-09-25 09:11:40 +0100

    Configure action cable for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://viniciuspc-message-me.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://viniciuspc-message-me.herokuapp.com', 'http://viniciuspc-message-me.herokuapp.com'] 
 end
